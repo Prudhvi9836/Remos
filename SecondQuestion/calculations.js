3fc03f2970d82952c41e27e92b85dd58229cfb85
Jayanth K <jayanthk@maveric-systems.com>
2023-07-25 17:07:48 +0530

    1 2 3 5

--- SecondQuestion/calculations.js
+++ SecondQuestion/calculations.js
@@ -1,14 +1,29 @@
 function updateTotal() {
-    const productSelect = document.getElementById("productSelect");
-    const quantityInput = document.getElementById("quantityInput");
+    const productCheckboxes = document.getElementsByName("product");
+    const quantityInputs = document.getElementsByName("quantity");
     const totalAmount = document.getElementById("totalAmount");
-    const selectedProductPrice = parseInt(productSelect.value);
-    const quantity = parseInt(quantityInput.value);
-    var total = selectedProductPrice * quantity;
-    total = total>1000?total-(total/10):total
-    totalAmount.innerHTML = `Total Amount: $${total}`;
+    let total = 0;
+
+    for (let i = 0; i < productCheckboxes.length; i++) {
+        if (productCheckboxes[i].checked) {
+            const selectedProductPrice = parseInt(productCheckboxes[i].value);
+            const quantity = parseInt(quantityInputs[i].value);
+            total += selectedProductPrice * quantity;
+        }
+    }
+    if(total<=1000){
+        totalAmount.innerHTML = `Total Amount: $${total}`;
+    }
+    else{
+        totalAmount.innerHTML = `Discounted Total Amount $${total-(total/10)}`;
+    }
 }
 
 function calculateTotal() {
+    // For demonstration purposes, this function is triggered by the "Calculate Total" button,
+    // but you can use it to submit the form to a server and perform calculations there.
+    // You can use JavaScript fetch or AJAX to send data to the server and get the total amount in response.
+    // Here, we will simply call the updateTotal() function to calculate the total and display it.
+
     updateTotal();
 }
